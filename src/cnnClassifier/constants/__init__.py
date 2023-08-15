037bb6dadfb1af21e2b5dacd9203d00d13b648dd
weedu34 <weedu34@gmail.com>
2023-08-15 13:25:46 +0200

    data ingestion step

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("C:/Users/Student/Chicken_Disease_Classification/config/config.yaml")
+PARAMS_FILE_PATH = Path("C:/Users/Student/Chicken_Disease_Classification/params.yaml")
